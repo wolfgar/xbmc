54befd8bec6f9c1f2b9879bb3b00acd3b10b8f4f
wolfgar <stephan.rafin@laposte.net>
2013-06-12 23:00:03 +0200

    Remove linux event input periodic checks These checks induce very visible lag every 10 seconds Additional investigation is required...

--- xbmc/input/linux/LinuxInputDevices.cpp
+++ xbmc/input/linux/LinuxInputDevices.cpp
@@ -1263,7 +1263,8 @@ XBMC_Event CLinuxInputDevices::ReadEvent()
 
     if ((now - m_lastHotplugCheck) >= 10)
     {
-      CheckHotplugged();
+      // Comment for now as it has visible issue on video decoding when USB devices are available
+      //CheckHotplugged();
       m_lastHotplugCheck = now;
     }
   }
